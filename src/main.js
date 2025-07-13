382dd3600e344d7dae1c1b81f23b5960cd1e90e9
hero2828 <719857499@qq.com>
2025-07-13 19:42:50 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,15 +1,10 @@
 import { Client, Users,Databases } from 'node-appwrite';
-import { Models } from "appwrite";
 
-interface RequestBody extends Models.User<Models.Preferences> {
-  targets: Models.Target[];
-  accessedAt: string;
-}
 // This Appwrite function will be executed every time your function is triggered
 export default async ({ req, res, log, error }) => {
   // You can use the Appwrite SDK to interact with other services
   // For this example, we're using the Users service
-   const data: RequestBody = req.bodyJson;
+   const data = req.bodyJson;
   const client = new Client()
     .setEndpoint(process.env.APPWRITE_FUNCTION_API_ENDPOINT)
     .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
